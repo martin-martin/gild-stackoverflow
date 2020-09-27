08ecb117ef1fab344cc40d46831b243aaaf2e56a
martin-martin <breuss.martin@gmail.com>
2020-09-27 14:53:49 +0300

    Add changes

--- gold_badge.py
+++ gold_badge.py
@@ -1,12 +1,12 @@
 import os
-import datetime
+import time
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-def auto_login(driver, email, password):
-    # open the login page
-    driver.get('https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f')
+def auto_login(driver, email, password, URL):
+    driver.get(URL)
     # log in: email, password, click
     driver.find_element_by_xpath("//input[@id='email']").send_keys(email)
     # selecting a field to TAB forward from
@@ -20,20 +20,48 @@ def auto_login(driver, email, password):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='gol.log',
+                        format='%(asctime)s - %(message)s',
+                        level=logging.INFO,
+                        )
+
+    stackoverflow = 'https://stackoverflow.com/users/login?ssrc=head&returnurl=https%3a%2f%2fstackoverflow.com%2f'
+    URLs = {
+        'gis': 'https://gis.stackexchange.com/',
+        'open-data': 'https://opendata.stackexchange.com/',
+        'cross-validated': 'https://stats.stackexchange.com/',
+        'data-science': 'https://datascience.stackexchange.com/',
+        'travel': 'https://travel.stackexchange.com/',
+        'bitcoin': 'https://bitcoin.stackexchange.com/',
+    }
     # declare path to chromedriver executable (located in same folder)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(chrome_options=chrome_options,
-                                executable_path='{0}/chromedriver'.format(dir_path))
-    # get credentials from open-source-safe environment variables
-    email = os.environ.get('SO_USR')
-    password = os.environ.get('SO_PWD')
-    # print(email, password)  # check whether the credentials are imported
+    #chrome_options.add_argument('--headless')
 
-    driver = auto_login(driver, email, password)
-    # logging
-    with open("gold.log", 'a') as f:
-        log = "{0} : successful login\n".format(datetime.datetime.now())
-        f.write(log)
+    # for now, just pop in the credentials here
+    email, password = 'breuss.martin@gmail.com', 'things to learn 8'
+
+    try:
+        driver = webdriver.Chrome(chrome_options=chrome_options,
+                                  executable_path='{0}/chromedriver'.format(dir_path))
+        try:
+            driver = auto_login(driver, email, password, stackoverflow)
+            logging.info('Logged in @ StackOverflow')
+            time.sleep(3)
+        except Exception as e:
+            logging.warning('FAILED Login @ SO: {0}'.format(e))
+        else:
+            for platform, url in URLs.items():
+                try:
+                    driver.get(url)
+                    logging.info('Success @ {0}'.format(platform))
+                except Exception as e:
+                    logging.warning('Failed @ {0}: {1}'.format(platform, e))
+                finally:
+                    time.sleep(3)
+    except Exception as e:
+        logging.warning('SCRIPT FAILED: {0}'.format(e))
+    finally:
+        driver.quit()
